4f4719ff7dd9de18d4ce4bc831426ee577df11cb
Aakash Sundaresan <sundaresan@intueor.com>
2025-10-30 15:34:10 -0700

    amplify auth added

--- react-app-mca/mca-portal/src/index.js
+++ react-app-mca/mca-portal/src/index.js
@@ -1,8 +1,12 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
 import './index.css';
 import App from './App';
+
+Amplify.configure(awsconfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
